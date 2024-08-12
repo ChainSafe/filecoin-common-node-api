17ac5a8fb176e7ecf96fca660f37956054a8123b
Aatif Syed <aatifsyedyp@gmail.com>
2024-08-12 17:30:56 +0100

    wip

--- tool/src/test_suite.rs
+++ tool/src/test_suite.rs
@@ -2,7 +2,7 @@ use crate::test::{test, Context as _, Ctx, Tag, Test};
 
 pub fn test_suite() -> Vec<Test<'static>> {
     [test("chain head", |ctx| {
-        ctx.tag(Tag::Basic);
+        ctx.tag(Tag::SchemaCoverage);
         ctx.call::<crate::test::types::Tipset>("Filecoin.ChainHead", ())?;
         Ok(())
     })]
